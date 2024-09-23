90b0feb22373683b7bf36207242faffa7933648b
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-09-23 19:19:18 -0300

    Updatejs04

--- Aulas teste/revi JS/04.js
+++ Aulas teste/revi JS/04.js
@@ -1,9 +1,19 @@
-// Proposições
-const proposicoes = [
-    "O céu é azul.",  // proposição
-    "Abra a porta.",  // não é proposição
-    "2 + 2 = 4.",     // proposição
-    "Vamos ao cinema amanhã" // não é proposição
-];
+// Função para verificar se a frase é uma proposição
+var frase1 = "O céu é azul.";
+var frase2 = "Abra a porta.";
+var frase3 = "2 + 2 = 4.";
+var frase4 = "Vamos ao cinema amanhã";
 
-console.log(proposicoes)
+function verificarProposicao(sentenca) {
+    
+    if (sentenca.includes("=") || (sentenca.endsWith(".") && !sentenca.startsWith("Abra"))) {
+        return true; 
+    } else {
+        return false; 
+    }
+}
+
+console.log("Frase 1: " + verificarProposicao(frase1)); 
+console.log("Frase 2: " + verificarProposicao(frase2)); 
+console.log("Frase 3: " + verificarProposicao(frase3)); 
+console.log("Frase 4: " + verificarProposicao(frase4)); 
