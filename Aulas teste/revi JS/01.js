d158e491859cb887b016fb79d14277518893d76a
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-09-23 19:52:19 -0300

    01.js

--- Aulas teste/revi JS/01.js
+++ Aulas teste/revi JS/01.js
@@ -1,10 +1,12 @@
 // Escreva um código que verifique se uma pessoa pode votar (idade >= 18) e se é obrigatória a votação (idade >= 18 e idade <= 70).
 
 function podeVotar(idade) {
-    const podeVotar = idade >= 18;
-    const votoObrigatorio = idade >= 18 && idade <= 70;
-    
-    console.log(`Pode votar: ${podeVotar}`);
-    console.log(`Voto obrigatório: ${votoObrigatorio}`);
+    if(idade >= 18 && idade <= 70){
+        return ("Votação obrigatória")
+    }else if( idade > 70) {
+        return ("Opcional")
+    }else{
+        return ("Não pode")
+    }
 }
-podeVotar(20);
+console.log(podeVotar(20))
