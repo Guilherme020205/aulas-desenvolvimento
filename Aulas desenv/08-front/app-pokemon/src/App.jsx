08daccbb87e283cc1ff47e3b3131cf5cfe0bf7ff
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-09-26 20:05:28 -0300

    pokemon ok

--- Aulas desenv/08-front/app-pokemon/src/App.jsx
+++ Aulas desenv/08-front/app-pokemon/src/App.jsx
@@ -5,20 +5,35 @@ import axios from 'axios'
 function App() {
 
   const [pokemon, setPokemon] = useState(null)
+  const [idPokemon, setIdPokemon] = useState(0)
 
   const carregarPokemon = async () => {
 
-    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/1')
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
       setPokemon(response.data)
   }
-  
 carregarPokemon()
+
+const proximoPokemon = () => {
+
+  setIdPokemon(idPokemon + 1)
+
+}
+const anteriorPokemon = () => {
+
+  setIdPokemon(idPokemon - 1)
+
+}
 
   return (
     <>
       <h1>Pokédex do Paulinho</h1>
       <h3>{pokemon?.name}</h3>
       <img src={pokemon?.sprites.front_default} alt="" />
+
+      <button onClick={anteriorPokemon}>Anterior</button>
+      <button onClick={proximoPokemon}>Soma</button>
+
     </>
   )
 }
