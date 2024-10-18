ee7a9516239182b57e1d6d707e45acd53442fc29
Guilherme020205 <104910701+Guilherme020205@users.noreply.github.com>
2024-10-18 07:39:56 -0300

    Git cypress

---
+++ app-filmes-cypress/src/pages/Filme.jsx
@@ -0,0 +1,30 @@
+import { useParams } from "react-router-dom"
+import axios from 'axios'
+import { useState, useEffect } from "react"
+
+function Filme() {
+    const { id } = useParams()
+    const [filme, setFilme] = useState(null)
+
+    useEffect(() => {
+        buscarFilmePeloId(id)
+    }, [])
+
+    const buscarFilmePeloId = async (id) => {
+        const url = `https://www.omdbapi.com/?apikey=28d0dee8&i=${id}`
+        const resposta = await axios.get(url)
+        setFilme(resposta.data)
+    }
+
+    return (
+        <div>
+            <h1>{filme?.Title}</h1>
+            <img src={filme?.Poster} width={100} alt="Poster do Filme" />
+            <p>{filme?.Plot}</p>
+            <p>{filme?.Runtime}</p>
+            <p>{filme?.Director}</p>
+            <p>{filme?.Genre}</p>
+        </div>
+    )
+}
+export default Filme
